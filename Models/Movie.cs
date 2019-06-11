8f5c822044bf7c32acf91198d6d016ab1d1e41c7
soakes84 <spencer.oakes84@gmail.com>
2019-06-10 22:05:07 -0400

    initial

---
+++ Models/Movie.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+
+namespace DVDMovieStore.Models
+{
+    public class Movie
+    {
+        public long MovieId{get;set;}
+        public string Name {get;set;}
+        public string Category {get;set;}
+        public string Description{get;set;}
+        public decimal Price {get;set;}
+
+        public Studio Studio {get;set;}
+        public List<Rating> Ratings{get;set;}
+    }
+}
