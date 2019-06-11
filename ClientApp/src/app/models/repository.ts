0cf963170ca3f06e8963640650c4390e334c4e24
soakes84 <spencer.oakes84@gmail.com>
2019-06-10 23:48:42 -0400

    refactored some more

--- ClientApp/src/app/models/repository.ts
+++ ClientApp/src/app/models/repository.ts
@@ -2,12 +2,19 @@ import { Movie } from './movie.model';
 import { HttpClient } from '@angular/common/http';
 import { Inject, Injectable } from '@angular/core';
 
+const moviesUrl = '/api/movies';
 @Injectable()
 export class Repository {
 
 public movie: Movie;
-constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
-http.get<Movie>(baseUrl + 'api/SampleData/GetMovie').subscribe(result => {this.movie = result; },
+constructor(private http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
+http.get<Movie>(baseUrl + 'api/movies/GetMovie').subscribe(result => {this.movie = result; },
   error => console.error(error));
+
+  this.getMovie(3);
+}
+getMovie(id: number) {
+  this.http.get(moviesUrl + '/' + id).subscribe(response => { this.movie = response; });
+
 }
 }
